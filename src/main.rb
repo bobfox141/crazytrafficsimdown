12646c438c315ddb1f8067695a866d216bd849f4
Steven Garbeil <sgarbeil@gmail.com>
2024-06-19 12:19:58 -0500

    first simulation set up

--- src/main.rb
+++ src/main.rb
@@ -3,3 +3,9 @@
 # description
 # the launcher for the traffic light program. 
 # it sets the initial path and kicks off the timer.
+
+require "traffic-light.rb"
+
+puts "Beginning Traffic light simulation"
+light = TrafficLight.new
+light.go
--- src/traffic-light.rb
+++ src/traffic-light.rb
@@ -14,14 +14,29 @@
 # so, the traffic light is set up so it can interface directly with a 
 # display object, i.e. each invidual light is enabled. 
 
+require "timer.rb"
+
+RED = 0
+YELLOW = 1
+GREEN = 2
+
+
+
+BASIC = 0
+
+
 class TrafficLight
 
-	def initialize
-		@red = true
-		@yellow = false
-		@green = false
+	def initialize(type)
+		setRed()
+		@type = type
+		@done = false
 	end
-	
+
+	def killlight
+		@done = true
+	end
+
 	def setRed
 		@red = true
 		@yellow = false
@@ -44,23 +59,62 @@ class TrafficLight
 		if red == true then 
 			if yellow == false && green == false then 
 				return true
-			else
-				return false
 			end
 		elsif yellow == true then 
 			if red == false && green == false then
 				return true
-			else
-				return false
 			end
 		elsif green == true then 
 			if red == false && yellow == false then 
 				return true
-			else 
-				return false
 			end
 		end
-	end
+		return false 
 	
+	end
+
+	def setTimings
+		if @type == BASIC then 
+			return {RED => 60, YELLOW => 30, GREEN => 60 }
+		end 
+	end
+
+
+
+	def go
+		t = Timer.new
+		setRed()
+		timings = setTimings
+		seqbegin = t.now
+		seqswitch = seqbegin + timings[RED]
+		while not @done
+			current = t.now
+			if current >= seqswitch 
+				if @red == true then 
+					setGreen
+					seqswitch = seqbegin + timings[GREEN]
+					puts "Switching the active light to Green"
+				elsif @yellow == true then 
+					setRed
+					seqswitch = seqbegin + timings[RED]
+					puts "Switching the active light to Red, will switch again in #{current + timings[RED]}"
+				elsif @green == true then 
+					setYellow
+					seqswitch = seqbegin + timings[YELLOW]
+				end
+			else
+				sleep 0.2  # running at about 5hz 
+			end
+		end
+		puts "Loop ended, controller exiting."
+	end
+		
+	def showLight
+		puts "Red:    #{@red}"
+		puts "Yellow: #{@yellow}"
+		puts "Green:  #{@green}"
+
+		
+
 	
 end
